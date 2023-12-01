f9a1f2ab8802684f0ad4f55611f7e17b44ce60d1
Jakob Runge <github@runjak.codes>
2023-12-01 23:16:12 +0100

    move task_01 into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,6 @@
-use std::{error::Error, fs};
+use std::error::Error;
+mod task_01;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let paths = ["./inputs/01/example-1.txt", "./inputs/01/input.txt"];
-
-    for path in paths {
-        let file = fs::read_to_string(path)?;
-
-        println!("Reading file> {}", path);
-
-        let sum: u32 = file
-            .lines()
-            .filter_map(|line| -> Option<u32> {
-                let digits: Vec<_> = line
-                    .chars()
-                    .filter_map(|char: char| char.to_digit(10))
-                    .collect();
-
-                Some(digits.first()? * 10 + digits.last()?)
-            })
-            .sum();
-
-        println!("Sum of per-line values: {}", sum);
-    }
-
-    Ok(())
+    task_01::main()
 }
