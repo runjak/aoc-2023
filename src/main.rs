0be7be01e7d5252b18fbbc1e6ee4adfcb1519968
Jakob Runge <github@runjak.codes>
2023-12-01 23:04:28 +0100

    solve 01-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::{error::Error, fs};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let paths = ["./inputs/01/example.txt", "./inputs/01/input.txt"];
+
+    for path in paths {
+        let file = fs::read_to_string(path)?;
+
+        println!("Reading file> {}", path);
+
+        let sum: u32 = file
+            .lines()
+            .filter_map(|line| -> Option<u32> {
+                let digits: Vec<_> = line
+                    .chars()
+                    .filter_map(|char: char| char.to_digit(10))
+                    .collect();
+
+                Some(digits.first()? * 10 + digits.last()?)
+            })
+            .sum();
+
+        println!("Sum of per-line values: {}", sum);
+    }
+
+    Ok(())
 }
