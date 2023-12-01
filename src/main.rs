641fa5308d97cde4f747bb50236ae74e25f9a229
Jakob Runge <github@runjak.codes>
2023-12-01 23:36:01 +0100

    use env in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
-use std::error::Error;
+use std::{env, error::Error};
 mod task_01;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    task_01::main()
+    let args: Vec<_> = env::args()
+        .filter_map(|arg| arg.parse::<usize>().ok())
+        .collect();
+
+    let tasks = Vec::from([task_01::main]);
+
+    match args.first() {
+        None => {
+            println!("No task given.");
+            return Ok(());
+        }
+        Some(task_index) => match tasks.get(*task_index - 1) {
+            None => {
+                println!("No task for task_index {}.", *task_index);
+                return Ok(());
+            }
+            Some(task) => task(),
+        },
+    }
 }
