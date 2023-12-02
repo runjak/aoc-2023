c3b02258abc2abd5cb3321e5f6ec314481162909
Jakob Runge <github@runjak.codes>
2023-12-02 10:59:23 +0100

    work on task_02

--- src/task_02.rs
+++ src/task_02.rs
@@ -1,4 +1,11 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
+
+type Game = HashMap<str, u32>;
+type GameLog = Vec<(u32, Game)>;
+
+fn parse_game(line:&str) -> Game {
+  
+}
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     println!("02-1:");
