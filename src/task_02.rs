f887de4dcca2d50e85569d6a2e1821aeb3a7b6f5
Jakob Runge <github@runjak.codes>
2023-12-02 12:26:59 +0100

    test parse_game

--- src/task_02.rs
+++ src/task_02.rs
@@ -12,7 +12,7 @@ fn parse_game(line: &str) -> Option<Game> {
     }
 
     // We identify the id and drawing parts of a game:
-    let game_parts = line[prefix.len()..].split(":").collect::<Vec<_>>();
+    let game_parts = line[prefix.len()..].split(": ").collect::<Vec<_>>();
     let game_id = game_parts.get(0)?.parse::<u32>().ok()?;
     let drawing_strings = game_parts.get(1)?.split("; ").collect::<Vec<_>>();
 
@@ -20,14 +20,17 @@ fn parse_game(line: &str) -> Option<Game> {
     let drawings: Vec<Drawing> = drawing_strings
         .iter()
         .map(|drawing| -> Drawing {
-            return drawing.split(", ").filter_map(|part| -> Option<(String, u32)> {
-                let part_parts = part.split(" ").collect::<Vec<_>>();
+            return drawing
+                .split(", ")
+                .filter_map(|part| -> Option<(String, u32)> {
+                    let part_parts = part.split(" ").collect::<Vec<_>>();
 
-                let color = part_parts.get(1)?.to_string();
-                let count = part_parts.get(0)?.parse::<u32>().ok()?;
+                    let color = part_parts.get(1)?.to_string();
+                    let count = part_parts.get(0)?.parse::<u32>().ok()?;
 
-                Some((color, count))
-            }).collect::<Drawing>();
+                    Some((color, count))
+                })
+                .collect::<Drawing>();
         })
         .collect();
 
@@ -39,4 +42,27 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     println!("Work in progress.");
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::task_02::{parse_game, Drawing, Game};
+
+    #[test]
+    fn parse_game_parses_first_example_line() {
+        let example = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green";
+        
+        let drawings: Vec<Drawing> = Vec::from([
+            HashMap::from([(String::from("blue"), 3), (String::from("red"), 4)]),
+            HashMap::from([(String::from("red"), 1), (String::from("green"), 2), (String::from("blue"), 6)]),
+            HashMap::from([(String::from("green"), 2)]),
+        ]);
+        let expected: Option<Game> = Some((1, drawings));
+
+        let game = parse_game(example);
+
+        assert_eq!(game, expected);
+    }
 }
