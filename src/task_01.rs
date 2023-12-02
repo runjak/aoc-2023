507faff26ca3e9eff1c6bb7aac19b5125c501ef3
Jakob Runge <github@runjak.codes>
2023-12-02 01:19:13 +0100

    progress towards 01-2

--- src/task_01.rs
+++ src/task_01.rs
@@ -6,7 +6,7 @@ fn first() -> Result<(), Box<dyn Error>> {
     for path in paths {
         let file = fs::read_to_string(path)?;
 
-        println!("Reading file> {}", path);
+        println!("Reading file: {}", path);
 
         let sum: u32 = file
             .lines()
@@ -26,6 +26,70 @@ fn first() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn find_digits(line: &str) -> Vec<u32> {
+    let cases: Vec<(&str, usize)> = Vec::from([
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let mut line = &line[..];
+    let mut digits = Vec::new();
+
+    while line.len() > 0 {
+        for (prefix, digit) in &cases {
+            if line.starts_with(*prefix) {
+                digits.push(digit);
+                break;
+            }
+        }
+
+        line = &line[1..];
+    }
+
+    return Vec::new();
+}
+
+fn second() -> Result<(), Box<dyn Error>> {
+    let paths = ["./inputs/01/example-2.txt", "./inputs/01/input.txt"];
+
+    for path in paths {
+        let file = fs::read_to_string(path)?;
+
+        println!("Reading file: {}", path);
+        let sum: u32 = file
+            .lines()
+            .filter_map(|line| -> Option<u32> {
+                let digits = find_digits(line);
+
+                Some(digits.first()? * 10 + digits.last()?)
+            })
+            .sum();
+
+        println!("Sum of per-line values: {}", sum);
+    }
+
+    Ok(())
+}
+
 pub fn main() -> Result<(), Box<dyn Error>> {
-    first()
+    first()?;
+    second()
 }
