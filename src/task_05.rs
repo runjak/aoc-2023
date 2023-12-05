144fce745d8822372371680bb3ac3cc3d6f1d706
Jakob Runge <github@runjak.codes>
2023-12-05 19:42:18 +0100

    parse example for task 05

--- src/task_05.rs
+++ src/task_05.rs
@@ -1,7 +1,85 @@
-use std::error::Error;
+use regex::Regex;
+use std::{error::Error, fs};
+
+#[derive(Debug, PartialEq)]
+struct CategoryMap {
+    from: String,
+    to: String,
+    mappings: Vec<(u32, u32, u32)>,
+}
+
+#[derive(Debug, PartialEq)]
+struct TaskInput {
+    seeds: Vec<u32>,
+    category_maps: Vec<CategoryMap>,
+}
+
+fn parse_seeds(line: &str) -> Vec<u32> {
+    let prefix = "seeds: ";
+
+    if !line.starts_with(prefix) {
+        return Vec::new();
+    }
+
+    line[prefix.len()..]
+        .split(" ")
+        .filter_map(|digits| digits.parse::<u32>().ok())
+        .collect()
+}
+
+fn parse_category_map(lines: &str) -> Option<CategoryMap> {
+    let lines = &mut lines.lines();
+
+    let first_line = lines.next()?;
+    let first_line_regex = Regex::new(r"(?<from>[a-z]+)-to-(?<to>[a-z]+)\smap:").ok()?;
+    let captures = first_line_regex.captures(first_line)?;
+
+    let from = captures.name("from")?.as_str().to_string();
+    let to = captures.name("to")?.as_str().to_string();
+
+    let mappings = lines
+        .filter_map(|line| -> Option<(u32, u32, u32)> {
+            let numbers = line
+                .split(" ")
+                .filter_map(|digits| digits.parse::<u32>().ok())
+                .collect::<Vec<_>>();
+
+            let [x, y, z] = numbers.as_slice() else {
+                return None;
+            };
+
+            Some((*x, *y, *z))
+        })
+        .collect();
+
+    Some(CategoryMap { from, to, mappings })
+}
+
+fn parse_task_input(input: String) -> Option<TaskInput> {
+    let items = &mut input.split("\n\n");
+
+    let seeds = parse_seeds(items.next()?);
+    let category_maps = items.filter_map(parse_category_map).collect::<Vec<_>>();
+
+    Some(TaskInput {
+        seeds,
+        category_maps,
+    })
+}
 
 pub fn first() -> Result<(), Box<dyn Error>> {
-    println!("To be implemented");
+    let paths = ["./inputs/05/example-1.txt", "./inputs/05/input.txt"];
+
+    for path in paths {
+        println!("Reading file {}", path);
+
+        let contents = fs::read_to_string(path)?;
+        let task_input = parse_task_input(contents).unwrap();
+
+        println!("{:?}", task_input);
+
+        break;
+    }
 
     Ok(())
 }
