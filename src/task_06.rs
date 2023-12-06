7ebb3ecc5e4e05a4f69dc58d37235526c7596f86
Jakob Runge <github@runjak.codes>
2023-12-06 19:57:20 +0100

    solve 06-2

--- src/task_06.rs
+++ src/task_06.rs
@@ -2,11 +2,11 @@ use std::{error::Error, fs};
 
 #[derive(Debug)]
 struct Race {
-    time: i32,
-    distance: i32,
+    time: i64,
+    distance: i64,
 }
 
-fn parse_input(contents: String) -> Vec<Race> {
+fn parse_input_1(contents: String) -> Vec<Race> {
     let lines = contents.lines().collect::<Vec<_>>();
     let [line1, line2] = lines.as_slice() else {
         return Vec::new();
@@ -18,10 +18,10 @@ fn parse_input(contents: String) -> Vec<Race> {
 
     let times = line1
         .split(" ")
-        .filter_map(|token| token.parse::<i32>().ok());
+        .filter_map(|token| token.parse::<i64>().ok());
     let distances = line2
         .split(" ")
-        .filter_map(|token| token.parse::<i32>().ok());
+        .filter_map(|token| token.parse::<i64>().ok());
 
     times
         .zip(distances)
@@ -29,7 +29,7 @@ fn parse_input(contents: String) -> Vec<Race> {
         .collect()
 }
 
-fn count_ways_to_beat(race: &Race) -> i32 {
+fn count_ways_to_beat(race: &Race) -> i64 {
     let mut count = 0;
 
     for speed in 0..race.time {
@@ -49,17 +49,42 @@ fn first() -> Result<(), Box<dyn Error>> {
 
     for path in paths {
         let contents = fs::read_to_string(path)?;
-        let races = parse_input(contents);
+        let races = parse_input_1(contents);
 
-        let solution = races.iter().map(count_ways_to_beat).product::<i32>();
+        let solution = races.iter().map(count_ways_to_beat).product::<i64>();
         println!("Product of winnable races: {}", solution);
     }
 
     Ok(())
 }
 
+fn parse_input_2(contents: String) -> Option<Race> {
+    let lines = contents
+        .lines()
+        .map(|line| line.chars().filter(|c| c.is_digit(10)).collect::<String>())
+        .filter_map(|digits| digits.parse::<i64>().ok())
+        .collect::<Vec<_>>();
+
+    let [time, distance] = lines.as_slice() else {
+        return None;
+    };
+
+    Some(Race {
+        time: *time,
+        distance: *distance,
+    })
+}
+
 fn second() -> Result<(), Box<dyn Error>> {
-    println!("To be implemented");
+    let paths = ["./inputs/06/example-1.txt", "./inputs/06/input.txt"];
+
+    for path in paths {
+        let contents = fs::read_to_string(path)?;
+        let race = parse_input_2(contents).unwrap();
+
+        let solution = count_ways_to_beat(&race);
+        println!("Ways to win the race: {}", solution);
+    }
 
     Ok(())
 }
