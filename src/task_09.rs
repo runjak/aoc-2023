f2e988dce08677b17785312fc67b1460435b7779
Jakob Runge <jakob.runge@gridfuse.com>
2023-12-09 12:09:58 +0100

    parse input for 09

--- src/task_09.rs
+++ src/task_09.rs
@@ -1,6 +1,30 @@
-use std::error::Error;
+use std::{error::Error, fs};
+
+type Z = i32;
+
+fn parse_input(contents: String) -> Vec<Vec<Z>> {
+    contents
+        .lines()
+        .map(|line| {
+            line.split(" ")
+                .filter_map(|digits| digits.parse().ok())
+                .collect()
+        })
+        .collect()
+}
 
 fn first() -> Result<(), Box<dyn Error>> {
+    let paths = ["./inputs/09/example-1.txt", "./inputs/09/input.txt"];
+
+    for path in paths {
+        let contents = fs::read_to_string(path)?;
+        let input = parse_input(contents);
+
+        println!("Input:\n{:?}", input);
+
+        break;
+    }
+
     Ok(())
 }
 
