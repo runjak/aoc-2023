4877077589cecdfee5c28b972791eb79563fd943
Jakob Runge <jakob.runge@gridfuse.com>
2023-12-09 14:04:56 +0100

    work on 09

--- src/task_09.rs
+++ src/task_09.rs
@@ -13,6 +13,31 @@ fn parse_input(contents: String) -> Vec<Vec<Z>> {
         .collect()
 }
 
+fn derive(values: &Vec<Z>) -> Vec<Z> {
+    let mut tail = values.iter();
+    tail.next();
+
+    values.iter().zip(tail).map(|(a, b)| b - a).collect()
+}
+
+fn all_zero(values: &Vec<Z>) -> bool {
+    values.iter().all(|v| v == &0)
+}
+
+fn derives(values: &Vec<Z>) -> Vec<Vec<Z>> {
+    let mut ret = Vec::new();
+
+    let mut current_values = values.clone();
+    while !all_zero(&current_values) {
+        let next_values = derive(&current_values);
+        ret.push(current_values);
+        current_values = next_values;
+    }
+    ret.push(current_values);
+
+    return ret;
+}
+
 fn first() -> Result<(), Box<dyn Error>> {
     let paths = ["./inputs/09/example-1.txt", "./inputs/09/input.txt"];
 
@@ -20,7 +45,7 @@ fn first() -> Result<(), Box<dyn Error>> {
         let contents = fs::read_to_string(path)?;
         let input = parse_input(contents);
 
-        println!("Input:\n{:?}", input);
+        println!("Input:\n{:?}", derives(input.get(0).unwrap()));
 
         break;
     }
