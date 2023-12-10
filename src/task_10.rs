e6ad6d287e6807e4dcbc6be4ed686faf8b3c99c1
Jakob Runge <github@runjak.codes>
2023-12-10 14:36:00 +0100

    10: parse input

--- src/task_10.rs
+++ src/task_10.rs
@@ -1,7 +1,29 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error, fs};
+
+type Coordinate = (usize, usize);
+
+fn parse_input(input: String) -> HashMap<Coordinate, char> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| -> Vec<(Coordinate, char)> {
+            line.chars()
+                .enumerate()
+                .map(|(x, char)| -> (Coordinate, char) { ((x, y), char) })
+                .collect()
+        })
+        .collect()
+}
 
 fn first() -> Result<(), Box<dyn Error>> {
-    println!("To be implemented.");
+    let paths = ["./inputs/10/example-1.txt", "./inputs/10/example-2.txt"]; //, "./inputs/10/input.txt"];
+
+    for path in paths {
+        let input = fs::read_to_string(path)?;
+        let input = parse_input(input);
+
+        println!("Got input:\n{:?}", input);
+    }
 
     Ok(())
 }
