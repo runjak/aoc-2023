0331b7c6a797ad297e7627b824733830bdd02bd5
Jakob Runge <github@runjak.codes>
2023-12-16 10:31:04 +0100

    11: towards input parsing

--- src/task_11.rs
+++ src/task_11.rs
@@ -3,11 +3,24 @@ use std::{collections::HashMap, error::Error, fs};
 type Coordinate = (i32, i32);
 type Input = HashMap<Coordinate, char>;
 
+fn parse_input(contents: String) -> Input {
+    contents
+        .lines()
+        .enumerate()
+        .flat_map(|(row_index, line)| {
+            let y = i32::try_from(row_index).unwrap();
+
+            line.chars().enumerate().map(move |(col_index, char)| {
+                let x = i32::try_from(col_index).unwrap();
+
+                ((x, y), char)
+            })
+        })
+        .collect::<Input>()
+}
+
 fn first() -> Result<(), Box<dyn Error>> {
-    let paths = [
-        "./inputs/11/example-1.txt",
-        "./inputs/11/input.txt",
-    ];
+    let paths = ["./inputs/11/example-1.txt", "./inputs/11/input.txt"];
 
     println!("To be implemented.");
 
