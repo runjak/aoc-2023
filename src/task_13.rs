808ce63b987cfb19bf450b71962d65f70c6bad11
Jakob Runge <github@runjak.codes>
2023-12-24 18:17:05 +0100

    13: add transpose_pattern

--- src/task_13.rs
+++ src/task_13.rs
@@ -1,4 +1,37 @@
-use std::error::Error;
+use std::{error::Error, fs};
+
+// A pattern is made up of several lines of strings.
+type Pattern = Vec<String>;
+
+fn parse_input(contents: String) -> Vec<Pattern> {
+    contents
+        .split("\n\n")
+        .map(|lines| -> Pattern { lines.split("\n").map(|s| s.to_string()).collect::<Vec<_>>() })
+        .collect()
+}
+
+fn transpose_pattern(pattern: &Pattern) -> Pattern {
+    // Inspired by https://stackoverflow.com/questions/64498617/how-to-transpose-a-vector-of-vectors-in-rust
+
+    if pattern.is_empty() {
+        return Vec::new();
+    }
+
+    let width = pattern[0].len();
+    let mut iters = pattern
+        .into_iter()
+        .map(|line| line.chars())
+        .collect::<Vec<_>>();
+
+    (0..width)
+        .map(|_| {
+            iters
+                .iter_mut()
+                .filter_map(|i| i.next())
+                .collect::<String>()
+        })
+        .collect()
+}
 
 fn first() -> Result<(), Box<dyn Error>> {
     let paths = ["./inputs/13/example-1.txt", "./inputs/13/input.txt"];
@@ -6,7 +39,12 @@ fn first() -> Result<(), Box<dyn Error>> {
     for path in paths {
         println!("File {}", path);
 
-        println!("To be implemented.");
+        let contents = fs::read_to_string(path)?;
+        let input = parse_input(contents);
+
+        println!("Got inputs: {:?}", input);
+
+        break;
     }
 
     Ok(())
@@ -26,4 +64,3 @@ pub fn main() -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
-
