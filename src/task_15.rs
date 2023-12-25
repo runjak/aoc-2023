425ea9ef13274d0cfefa43323c1e6f427d1db903
Jakob Runge <github@runjak.codes>
2023-12-25 01:08:15 +0100

    15: add and test hash method

--- src/task_15.rs
+++ src/task_15.rs
@@ -1,5 +1,18 @@
 use std::{error::Error, fs};
 
+fn hash(input: &str) -> u8 {
+    let mut current_value: u8 = 0;
+
+    let ascii_chars = input.chars().filter_map(|c| u8::try_from(c).ok());
+
+    for c in ascii_chars {
+        current_value = current_value.wrapping_add(c);
+        current_value = current_value.wrapping_mul(17);
+    }
+
+    current_value
+}
+
 fn first() -> Result<(), Box<dyn Error>> {
     let paths = ["./inputs/15/example-1.txt", "./inputs/15/input.txt"];
 
@@ -33,4 +46,17 @@ pub fn main() -> Result<(), Box<dyn Error>> {
     second()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hash;
+
+    #[test]
+    fn hash_should_hash_single_example_string() {
+        let expected: u8 = 52;
+        let actual = hash("HASH");
+
+        assert_eq!(actual, expected);
+    }
 }
